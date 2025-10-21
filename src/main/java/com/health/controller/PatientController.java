5d18d3f9e911fc2a94fac120e13a50e58c8e6dbe
reusromero8 <reusromero8@gmail.com>
2025-10-21 13:07:03 -0500

    Change patient endpoint to api/patients
    Para la carga de los pacientes

--- src/main/java/com/health/controller/PatientController.java
+++ src/main/java/com/health/controller/PatientController.java
@@ -17,7 +17,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/patients")
+@RequestMapping("api/patients")
 //@AllArgsConstructor
 @RequiredArgsConstructor
 // @CrossOrigin(origins = "*")
